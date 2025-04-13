9a842c232f63e4598f3d3eb6bb956174996d36e0
Racika <kalwandvok@gmail.com>
2025-04-13 17:49:55 +0200

    angular-depends

---
+++ .angular/cache/19.2.7/tatto-parlor/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,15 @@
+import "./chunk-M3HR6BUY.js";
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-YHR4ERWU.js";
+import "./chunk-O44BUVQZ.js";
+import "./chunk-FHYSXNDI.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ .angular/cache/19.2.7/tatto-parlor/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,46 @@
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-OK2X6PEE.js";
+import "./chunk-LLSYBTIE.js";
+import "./chunk-6RP2MNTD.js";
+import {
+  Dir
+} from "./chunk-YHR4ERWU.js";
+import "./chunk-O44BUVQZ.js";
+import "./chunk-FHYSXNDI.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
